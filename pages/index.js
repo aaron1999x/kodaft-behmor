cf89aa972184e8a905384510e3dcf28d8c1e7795
AaronTan <aarontys1999@gmail.com>
2023-05-18 23:20:29 +0800

    change index.js to blank

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,8 @@
-import { Raleway,Playfair_Display } from 'next/font/google'
-import Hero from '@/components/Hero'
-import Header from '@/components/Header'
-import Features from '@/components/Features'
-import WhatsappCTA from '@/components/WhatsappCTA'
-import MetaHead from '@/components/MetaHead'
-
-const raleway = Raleway({ subsets: ['latin'] })
-const playfair =Playfair_Display({ subsets: ['latin'],variable:'--font-playfair' })
 
 export default function Home() {
   return (
-    <main
-      className={`${raleway.className} ${playfair.variable} relative`}
-    >
-      <MetaHead />
-      <Header/>
-      <Hero />
-      <Features />
-      <WhatsappCTA />
-    </main>
+    <>
+     I love coffee
+    </>
   )
 }
