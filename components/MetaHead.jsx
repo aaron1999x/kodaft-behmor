03b8274373595493622b50f4ec5cdeb7f174c20f
AaronTan <aarontys1999@gmail.com>
2023-05-20 03:58:57 +0800

    update meta tags

--- components/MetaHead.jsx
+++ components/MetaHead.jsx
@@ -6,22 +6,22 @@ const MetaHead = () => {
     <Head>
       {/* <!-- Primary Meta Tags --> */}
       <title>Behmor Plus 2020SR | Kodaft</title>
-      <meta name="title" content="Behmor Plus 2020SR | Kodaft"/>
+      <meta name="title" content="Behmor Plus 2020SR Coffee Roaster | Kodaft"/>
       <meta name="description" content="Behmor 2020SR Plus is an award-winning coffee roaster enables craft coffee roasting to be experienced at home with precision controls and smoke suppression."/>
 
       {/* <!-- Open Graph / Facebook --> */}
       <meta property="og:type" content="website"/>
-      {/* <meta property="og:url" content="https://metatags.io/"/> */}
-      <meta property="og:title" content="Behmor Plus 2020SR | Kodaft"/>
+      <meta property="og:url" content="https://kodaft.com/behmorcoffeeroaster"/>
+      <meta property="og:title" content="Behmor Plus 2020SR Coffee Roaster | Kodaft"/>
       <meta property="og:description" content="Behmor 2020SR Plus is an award-winning coffee roaster enables craft coffee roasting to be experienced at home with precision controls and smoke suppression."/>
-      {/* <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/> */}
+      <meta property="og:image" content="https://kodaft.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbehmor-machine.1eaa61fa.png&w=1080&q=75"/>
 
       {/* <!-- Twitter --> */}
       <meta property="twitter:card" content="summary_large_image"/>
-      {/* <meta property="twitter:url" content="https://metatags.io/"/> */}
-      <meta property="twitter:title" content="Behmor Plus 2020SR | Kodaft"/>
+      <meta property="twitter:url" content="https://kodaft.com/behmorcoffeeroaster"/>
+      <meta property="twitter:title" content="Behmor Plus 2020SR Coffee Roaster | Kodaft"/>
       <meta property="twitter:description" content="Behmor 2020SR Plus is an award-winning coffee roaster enables craft coffee roasting to be experienced at home with precision controls and smoke suppression."/>
-      {/* <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/> */}
+      <meta property="twitter:image" content="https://kodaft.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbehmor-machine.1eaa61fa.png&w=1080&q=75"/>
     </Head>
   )
 }
