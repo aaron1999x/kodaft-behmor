cced9371fe526b657ad182334cc0338f57df003f
AaronTan <aarontys1999@gmail.com>
2023-05-22 20:42:04 +0800

    add google tag

--- pages/behmorcoffeeroaster.js
+++ pages/behmorcoffeeroaster.js
@@ -4,6 +4,7 @@ import Header from '@/components/Header'
 import Features from '@/components/Features'
 import WhatsappCTA from '@/components/WhatsappCTA'
 import MetaHead from '@/components/MetaHead'
+import Script from 'next/script'
 
 const raleway = Raleway({ subsets: ['latin'] })
 const playfair =Playfair_Display({ subsets: ['latin'],variable:'--font-playfair' })
@@ -14,6 +15,19 @@ const behmorcoffeeroaster = () => {
     <main
       className={`${raleway.className} ${playfair.variable} relative`}
     >
+      <Script
+        src='https://www.googletagmanager.com/gtag/js?id=G-GQSDTSM775'
+        strategy="afterInteractive"
+        />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-GQSDTSM775');
+        `}
+      </Script>
       <MetaHead />
       <Header/>
       <Hero />
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Player } from '@lottiefiles/react-lottie-player';
+import Script from "next/script";
 
 export default function Home() {
 
@@ -8,13 +9,28 @@ export default function Home() {
   },[])
 
   return (
-    <div className="h-screen flex items-center justify-center">
-      <Player 
-        src='https://assets10.lottiefiles.com/packages/lf20_4mu3hoco.json'
-        className="player"
-        loop
-        autoplay
-      />
-    </div>
+    <>
+      <Script
+        src='https://www.googletagmanager.com/gtag/js?id=G-GQSDTSM775'
+        strategy="afterInteractive"
+        />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-GQSDTSM775');
+        `}
+      </Script>
+      <div className="h-screen flex items-center justify-center">
+        <Player 
+          src='https://assets10.lottiefiles.com/packages/lf20_4mu3hoco.json'
+          className="player"
+          loop
+          autoplay
+        />
+      </div>
+    </>
   )
 }
