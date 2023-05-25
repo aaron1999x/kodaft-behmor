d8d4f6cb5d55e70dc4b9e41a29d3a5c33e688898
AaronTan <aarontys1999@gmail.com>
2023-05-25 10:42:53 +0800

    temporary dummy page

---
+++ pages/behmorcoffeeroaster.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Player } from '@lottiefiles/react-lottie-player';
+import Script from "next/script";
+
+
+const behmorcoffeeroaster = () => {
+
+  React.useEffect(()=>{
+    window.location.href="/behmorcoffeeroastermalaysia"
+  },[])
+  
+  return (
+    <>
+    <Script
+      src='https://www.googletagmanager.com/gtag/js?id=G-GQSDTSM775'
+      strategy="afterInteractive"
+      />
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', 'G-GQSDTSM775');
+      `}
+    </Script>
+    <div className="h-screen flex items-center justify-center">
+      <Player 
+        src='https://assets10.lottiefiles.com/packages/lf20_4mu3hoco.json'
+        className="player"
+        loop
+        autoplay
+      />
+    </div>
+  </>
+  )
+}
+
+export default behmorcoffeeroaster
